1fa1c0b12e12fcff464142cf57614e2e40271a91
Yash Shrivastava <yash.shrivastavaAUG@gmail.com>
2023-11-03 15:26:52 +0530

    publish your course page added

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -14,22 +14,22 @@ export default function AddCourse() {
         </div>
         {/* Course Upload Tips */}
         <div className="sticky top-10 hidden max-w-[400px] flex-1 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6 xl:block">
-          <p className="mb-8 text-lg text-richblack-5">⚡ Course Upload Tips</p>
+          <p className="mb-8 text-lg text-richblack-5">Guidelines for Uploading Your Course</p>
           <ul className="ml-5 list-item list-disc space-y-4 text-xs text-richblack-5">
-            <li>Set the Course Price option or make it free.</li>
-            <li>Standard size for the course thumbnail is 1024x576.</li>
-            <li>Video section controls the course overview video.</li>
-            <li>Course Builder is where you create & organize a course.</li>
+            <li>You can choose to set a price for the course.</li>
+            <li>The standard size for the course thumbnail is 1024x576 pixels.</li>
+            <li>The video section allows you to upload and control the course overview video.</li>
             <li>
-              Add Topics in the Course Builder section to create lessons,
-              quizzes, and assignments.
+              Use the Course Builder section to structure and organize your course content.</li>
+            <li>
+            In the Course Builder, add topics to create lessons, quizzes, and assignments.
             </li>
             <li>
-              Information from the Additional Data section shows up on the
-              course single page.
+            In Additional Data, input info for course page; add context and extra details to your course.
             </li>
-            <li>Make Announcements to notify any important</li>
-            <li>Notes to all enrolled students at once.</li>
+            <li>
+Use Announcements for essential course updates, event notifications, and changes to inform enrolled students.</li>
+            <li>Send messages to all enrolled students at once.</li>
           </ul>
         </div>
       </div>
